8fd6a4cb0666cafc9c40c5a598b5f9bfd3d6acc4
piesyy <69423933+piesyy@users.noreply.github.com>
2021-05-23 18:06:59 -0400

    Update list.js

--- js/list.js
+++ js/list.js
@@ -123,6 +123,12 @@ const d = {
 				       "percent": 100,
 				       "hz": "240hz"
 			       },
+		{
+				       "user": "Koozoo2000",
+				       "link": "https://youtu.be/Q_AXunN3xcI",
+				       "percent": 100,
+				       "hz": "144hz"
+			       },
                        ],
                        "name": "ZAPHKIEL",
                        "author": "piesy",
@@ -155,9 +161,177 @@ const d = {
                        "id": 37456092,
                        "pass": "who cares",
                        "percentToQualify": 50,
-                       "verificationVid": "https://youtu.be/b6Iwuhj4Fz0",
+                       "verificationVid": "https://youtu.be/EB7cMXqgxtY",
                        "key": 8
                 },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              	{
+				       "user": "luna",
+				       "link": "https://youtu.be/BrEFZtgkT20",
+				       "percent": 100,
+				       "hz": "240hz"
+			       },
+                       ],
+                       "name": "Catalyze",
+                       "author": "piesy",
+                       "more": "none",
+                       "id": 50333567,
+                       "pass": "who cares",
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/FbklH71ci3E",
+                       "key": 9
+                },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Cadrega City",
+                       "author": "piesy",
+                       "more": "none",
+                       "id": 37255449,
+                       "pass": "who cares",
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/4mQiftM0FmU",
+                       "key": 10
+                },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Omicron",
+                       "author": "piesy",
+                       "more": "none",
+                       "id": 63560498,
+                       "pass": "who cares",
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/21_zuwjNww4",
+                       "key": 11
+                },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Delta Interface",
+                       "author": "piesy",
+                       "more": "none",
+                       "id": 45094581,
+                       "pass": "who cares",
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/lTMyzxnSBjE",
+                       "key": 12
+                },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Artificial Ascent",
+                       "author": "piesy",
+                       "more": "none",
+                       "id": 27122654,
+                       "pass": "who cares",
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/P81cn_3wIRc",
+                       "key": 13
+                },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Auditory Breaker",
+                       "author": "piesy",
+                       "more": "none",
+                       "id": 51791710,
+                       "pass": "who cares",
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/6efY1prdk28",
+                       "key": 14
+                },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Glowy",
+                       "author": "piesy",
+                       "more": "none",
+                       "id": 37361518,
+                       "pass": "who cares",
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/7jIdgDBm0DM",
+                       "key": 15
+                },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              	{
+				       "user": "Vones",
+				       "link": "https://youtu.be/ee9WvF_HUDs",
+				       "percent": 100,
+				       "hz": "300hz"
+			       },
+		{
+				       "user": "Koozoo2000",
+				       "link": "https://youtu.be/IIYDlVXcaVI",
+				       "percent": 100,
+				       "hz": "144hz"
+			       },
+                       ],
+                       "name": "Rate Demon",
+                       "author": "piesy",
+                       "more": "none",
+                       "id": 44860762,
+                       "pass": "who cares",
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/RZWhnT-0Wl8",
+                       "key": 16
+                },
+        /*=================================================================================*/
+		{
+                       "vids": [
+              	{
+				       "user": "Koozoo2000",
+				       "link": "https://youtu.be/7zqJQJvVeY4",
+				       "percent": 100,
+				       "hz": "144hz"
+			       },
+                       ],
+                       "name": "Ziroikabi",
+                       "author": "piesy",
+                       "more": "none",
+                       "id": 44773500,
+                       "pass": "who cares",
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/WYDV_31YsL4",
+                       "key": 17
+                },
+		
+        /*=================================================================================*/
+			{
+                       "vids": [
+              	{
+				       "user": "JT",
+				       "link": "https://youtu.be/bvObypx7roQ",
+				       "percent": 100,
+				       "hz": "144hz"
+			       },
+                       ],
+                       "name": "Surge of the Shield",
+                       "author": "piesy",
+                       "more": "none",
+                       "id": 61127840,
+                       "pass": "who cares",
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/hyPWWN-Kf9U",
+                       "key": 18
+                },
+		
         /*=================================================================================*/
 	],
 	"version": [
