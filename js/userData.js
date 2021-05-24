551392c40bf1056b93f6b3252e213f5e59a772a8
piesyy <69423933+piesyy@users.noreply.github.com>
2021-05-23 21:43:08 -0400

    Update userData.js

--- js/userData.js
+++ js/userData.js
@@ -94,7 +94,7 @@ function getUserData(user) {
     }
 
     for (var i = 0 ; i < user_data[user].progress.length ; i++) {
-        progresses = progresses + '<li><a href="'+user_data[user].progress[i].link+'" target="blank_">' + user_data[user].progress[i].map + ' ' + user_data[user].progress[i].progress + '% </strong>(#'+user_data[user].progress[i].rank+' / UP: '+user_data[user].progress[i].score+''+(parseInt(user_data[user].progress[i].hz.replace("hz", "")) >= 120 ? '' : ' / '+user_data[user].progress[i].hz)+')<strong></a></li>'
+        progresses = progresses + '<li><a href="'+user_data[user].progress[i].link+'" target="blank_">' + user_data[user].progress[i].map + ' ' + user_data[user].progress[i].progress + '% </strong>(#'+user_data[user].progress[i].rank+' / UP: '+user_data[user].progress[i].score+''+(parseInt(user_data[user].progress[i].hz.replace("hz", "")) >= 120 ? '' : ''+user_data[user].progress[i].hz)+')<strong></a></li>'
         if (user_data[user].progress[i].progress == 100) {
             clears++;
         }
