4b4e2a61df1307c5028f75e1bfb761348086f0d5
piesyy <69423933+piesyy@users.noreply.github.com>
2021-05-23 20:24:31 -0400

    Update userData.js

--- js/userData.js
+++ js/userData.js
@@ -17,7 +17,7 @@ function userList() {
             }
         }
     
-        var p = roundNumber(getUserPoint(i+1, 100, entry.percentToQualify, "144hz") * 1.5, 3);
+        var p = roundNumber(getUserPoint(i+1, 100, entry.percentToQualify, "144hz") * 1, 3);
         if (checkVerify == true) {
             for (var b = 0 ; b < user_data.length ; b++) {
                 var user_name = user_data[b].name.toUpperCase(); var data_name = verifier.toUpperCase();
@@ -90,7 +90,7 @@ function getUserData(user) {
     for (var i = 0 ; i < user_data[user].verified.length ; i++) {
         rank = user_data[user].verified[i] - 1;
         clears++;
-        progresses = progresses + '<li>' + list[rank].name + ' Verified </strong>(#'+(rank+1)+' / UP: '+roundNumber(getUserPoint(rank+1, 100, list[rank].percentToQualify, "144hz")*1.5, 3)+')<strong></a></li>'
+        progresses = progresses + '<li>' + list[rank].name + ' Verified </strong>(#'+(rank+1)+' / UP: '+roundNumber(getUserPoint(rank+1, 100, list[rank].percentToQualify, "144hz")*1, 3)+')<strong></a></li>'
     }
 
     for (var i = 0 ; i < user_data[user].progress.length ; i++) {
